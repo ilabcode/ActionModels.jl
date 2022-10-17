9c5896477015ed92ca958b10dbd877f69d0fbb54
Peter Thestrup Waade <peter@waade.net>
2022-10-17 12:10:19 +0200

    specs -> config

--- src/premade_models/premade_agents.jl
+++ src/premade_models/premade_agents.jl
@@ -1,22 +1,22 @@
-function premade_rw_softmax(specs::Dict)
+function premade_rw_softmax(config::Dict)
 
     #Default parameters and settings
-    default_specs =
+    default_config =
         Dict("learning_rate" => 1, "softmax_action_precision" => 1, "initial_value" => 1)
 
     #Warn the user about used defaults and misspecified keys
-    warn_premade_defaults(default_specs, specs)
+    warn_premade_defaults(default_config, config)
 
     #Merge to overwrite defaults
-    specs = merge(default_specs, specs)
+    config = merge(default_config, config)
 
     ## Create agent 
     action_model = binary_rw_softmax
     params = Dict(
-        "learning_rate" => specs["learning_rate"],
-        "softmax_action_precision" => specs["softmax_action_precision"],
+        "learning_rate" => config["learning_rate"],
+        "softmax_action_precision" => config["softmax_action_precision"],
     )
-    states = Dict("value" => specs["initial_value"], "action_probability" => missing)
+    states = Dict("value" => config["initial_value"], "action_probability" => missing)
     settings = Dict()
 
     return init_agent(action_model, params = params, states = states, settings = settings)
