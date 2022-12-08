9661ec12c61ce15c485ced5a7d315f1d883c8f06
Peter Thestrup Waade <peter@waade.net>
2022-12-08 16:02:43 +0100

    minor edit to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,10 @@
 using ActionModels
-#using HierarchicalGaussianFiltering
 using Test
-using CSV
-using DataFrames
-using Turing
-using Plots
-using StatsPlots
+# using CSV
+# using DataFrames
+# using Plots
+# using StatsPlots
 
-@testset "unit tests" begin
-    
-    #Check premade agents
-    include("test_premade_agent.jl")
-
+@testset "quick tests" begin
+    include("quick_tests.jl")
 end
-
-
-# @testset "HGF tests" begin
-    
-#     #Set up path for tutorials
-#     actionmodels_path = dirname(dirname(pathof(ActionModels)))
-#     tutorials_path = actionmodels_path * "/docs/tutorials/" 
-
-#     # include(tutorials_path * "hgf_tutorials/" * "classic_binary.jl")
-#     # include(tutorials_path * "hgf_tutorials/" *"classic_usdchf.jl")
-
-# end
