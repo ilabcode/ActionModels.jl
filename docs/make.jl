db36818bd5d1adeacd91985a4edca800628b64ea
Anna280 <54855627+Anna280@users.noreply.github.com>
2023-02-22 14:54:38 +0100

    quick test of documentation and corrections

--- docs/make.jl
+++ docs/make.jl
@@ -7,25 +7,23 @@ using Literate
 for filename in readdir("docs/src/generated_markdown_files")
     rm("docs/src/generated_markdown_files/" * filename)
 end
-#remove index file outside of the generated_markdown_files folder
-rm("docs/src/index.md")
 
 #Make Julia source files to markdowns
 for filename in readdir("docs/src/julia_src_files")
     if endswith(filename, ".jl")
         #Place the index file in another folder than the rest of the documentation
-        if startswith(filename,"index")
+        if startswith(filename, "index")
             Literate.markdown(
                 "docs/src/Julia_src_files/" * filename,
                 "docs/src",
                 documenter = true,
             )
         else
-        Literate.markdown(
-            "docs/src/julia_src_files/" * filename,
-            "docs/src/generated_markdown_files",
-            documenter = true,
-        )
+            Literate.markdown(
+                "docs/src/julia_src_files/" * filename,
+                "docs/src/generated_markdown_files",
+                documenter = true,
+            )
         end
     end
 end
--- test/fitting_tests.jl
+++ test/fitting_tests.jl
@@ -8,14 +8,14 @@ agent = premade_agent("premade_binary_rw_softmax")
 
 param_priors = Dict("learning_rate" => Uniform(0, 1))
 
-inputs = [1, 0, 1,1,1,1,1,0,0,0,0]
+inputs = [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0]
 
 actions = give_inputs!(agent, inputs)
 
 chains = fit_model(agent, param_priors, inputs, actions, n_chains = 1, n_iterations = 10)
 
-plot(fitted_model)
+plot(chains)
 
-plot_parameter_distribution(fitted_model, priors)
+plot_parameter_distribution(chains, param_priors)
 
-get_posteriors(fitted_model)
+get_posteriors(chains)
