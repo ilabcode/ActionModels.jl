421b5f68c05595912bc8f1fdf805fd9b9de12e91
Peter Thestrup Waade <peter@waade.net>
2023-05-11 15:57:24 +0100

    format

--- test/testsuite/HGF_tests.jl
+++ test/testsuite/HGF_tests.jl
@@ -4,13 +4,10 @@ using Distributions
 
 agent = premade_agent("hgf_gaussian_action")
 
-priors = Dict(
-    ("x1", "evolution_rate") => Normal(-5, 1)
-)
+priors = Dict(("x1", "evolution_rate") => Normal(-5, 1))
 
 inputs = [1, 1.2, 1.4]
 
 actions = [1, 1.1, 1.5]
 
-fit_model(agent, priors, inputs, actions, n_iterations = 100, n_chains = 1)
-
+@test fit_model(agent, priors, inputs, actions, n_iterations = 100, n_chains = 1)
