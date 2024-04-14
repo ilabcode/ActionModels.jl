61848c5fd268fc7cdd822ee156fab3ec306a4d2b
Peter Thestrup Waade <peter@waade.net>
2024-04-14 03:58:04 +0200

    minro edit while HGF package is updated

--- test/testsuite/HGF_tests.jl
+++ test/testsuite/HGF_tests.jl
@@ -1,21 +1,21 @@
-using ActionModels
-using HierarchicalGaussianFiltering
-using Distributions
-using Plots
-using StatsPlots
+# using ActionModels
+# using HierarchicalGaussianFiltering
+# using Distributions
+# using Plots
+# using StatsPlots
 
-agent = premade_agent(
-    "hgf_gaussian",
-    premade_hgf("continuous_2level", verbose = false),
-    verbose = false,
-)
+# agent = premade_agent(
+#     "hgf_gaussian_action",
+#     premade_hgf("continuous_2level", verbose = false),
+#     verbose = false,
+# )
 
-priors = Dict(("x1", "volatility") => Normal(-5, 1))
+# priors = Dict(("x1", "volatility") => Normal(-5, 1))
 
-inputs = [1, 1.2, 1.4]
+# inputs = [1, 1.2, 1.4]
 
-actions = [1, 1.1, 1.5]
+# actions = [1, 1.1, 1.5]
 
-results = fit_model(agent, priors, inputs, actions, n_iterations = 100, n_chains = 1)
+# results = fit_model(agent, priors, inputs, actions, n_iterations = 100, n_chains = 1)
 
-plot_parameter_distribution(results, priors)
+# plot_parameter_distribution(results, priors)
