42a7643ac6e02537808aeebec674d5868c5b6a94
Malte Lau Petersen <maltelau@mlpetersen.dk>
2024-04-17 11:48:20 +0200

    add to TODO

--- src/fitting/create_statistical_model.jl
+++ src/fitting/create_statistical_model.jl
@@ -18,6 +18,7 @@ function _statistical_model_turingglm(
 
     # TODO: expand to multiple formulas
     # TODO: random intercepts (hint: construct model matrix somehow instead of modelmatrix(MixedEffects(@formula)), which expects y
+    # TODO: more functionality than turingGLM: random slopes / intercept-only model / better priors
     # TODO: better default priors / able to specify priors for all params
 
     # extract y, X and Z
