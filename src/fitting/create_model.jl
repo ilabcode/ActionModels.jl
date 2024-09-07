21d5789099e564f0bfc44f3bdc15b5fc9d026c42
Peter Thestrup Waade <peter@waade.net>
2024-09-07 12:08:32 +0100

    minor typing change

--- src/fitting/create_model.jl
+++ src/fitting/create_model.jl
@@ -7,7 +7,7 @@ function create_model(
     data::DataFrame;
     input_cols::Union{Vector{T1},T1},
     action_cols::Union{Vector{T2},T3},
-    grouping_cols::Union{Vector{T3},T3},
+    grouping_cols::Union{Vector{T3},T3} = Vector{String}(),
     track_states::Bool = false,
 ) where {T1<:Union{String,Symbol},T2<:Union{String,Symbol},T3<:Union{String,Symbol}}
 
