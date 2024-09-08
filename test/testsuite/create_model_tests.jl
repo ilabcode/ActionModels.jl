d36c7269ed30366f84f13c4ab4dc07dd6080592b
Peter Thestrup Waade <peter@waade.net>
2024-09-08 11:25:04 +0100

    upgraded tests

---
+++ test/testsuite/create_model_tests.jl
@@ -0,0 +1,325 @@
+
+### TODO:
+# Make check in full_model that returned agents_parameters is same length as inputs/actions
+# Benchmark the try-catch in full_model
+# Figure out helper function for extracting generated quantities (track_states true/false)
+# Fix typing in create_model and full_model to give concrete types XX
+# Make rename_chains also deal with missing actions
+# Investigate why missing actions can be crazy samples
+# full workflow: model comparison (PSIS)
+# use arraydist for multiple actions XX
+# consider using a submodel for the agent model
+
+
+
+using Test
+using ActionModels, DataFrames
+
+@testset "fitting tests" begin
+
+    ### SETUP ###
+    #Create dataframe
+    data = DataFrame(
+        inputs = rand([0, 1], 20),
+        inputs_2 = rand([0, 1], 20),
+        actions = rand([0, 1], 20),
+        actions_2 = rand([0, 1], 20),
+        ID = [repeat(["A"], 5); repeat(["B"], 5); repeat(["A"], 5); repeat(["B"], 5)],
+        category = [repeat(["X"], 10); repeat(["Y"], 10)],
+    )
+
+    #Create agent
+    agent = premade_agent("binary_rescorla_wagner_softmax")
+
+    #Set prior
+    prior = Dict(
+        "learning_rate" => LogitNormal(0.0, 1.0),
+        "action_precision" => truncated(Normal(0.0, 1.0), lower = 0),
+    )
+
+    #Set samplings settings
+    sampler = NUTS(-1, 0.65; adtype = AutoReverseDiff(; compile = true))
+    n_iterations = 10
+    n_chains = 2
+
+
+    # @testset "single agent" begin
+    #     #Extract inputs and actions from data
+    #     inputs = data[!, :inputs]
+    #     actions = data[!, :actions]
+
+    #     #Create model
+    #     model = create_model(agent, prior, inputs, actions)
+
+    #     #Fit model
+    #     fitted_model = sample(model, sampler, n_iterations, n_chains = n_chains)
+    # end
+
+    # @testset "simple statistical model" begin
+
+    #     #Create model
+    #     model = create_model(
+    #         agent,
+    #         prior,
+    #         data,
+    #         input_cols = :inputs,
+    #         action_cols = :actions,
+    #         grouping_cols = :ID,
+    #     )
+
+    #     #Fit model
+    #     fitted_model = sample(model, sampler, n_iterations, n_chains = n_chains)
+
+    #     #Rename chains
+    #     renamed_model = rename_chains(fitted_model, prior, data, :ID)
+
+    #     #Create model with tracking states
+    #     model_tracked = create_model(
+    #         agent,
+    #         prior,
+    #         data,
+    #         input_cols = :inputs,
+    #         action_cols = :actions,
+    #         grouping_cols = :ID,
+    #         track_states = true,
+    #     )
+
+    #     #Extract quantities
+    #     agent_parameters, agent_states, statistical_values =
+    #         extract_quantities(fitted_model, model_tracked)
+    # end
+
+    # @testset "multiple grouping cols" begin
+
+    #     #Create model
+    #     model = create_model(
+    #         agent,
+    #         prior,
+    #         data,
+    #         input_cols = :inputs,
+    #         action_cols = :actions,
+    #         grouping_cols = [:ID, :category],
+    #     )
+
+    #     #Fit model
+    #     fitted_model = sample(model, sampler, n_iterations, n_chains = n_chains)
+
+    #     #Rename chains
+    #     renamed_model = rename_chains(fitted_model, prior, data, [:ID, :category])
+
+    #     #Create model with tracking states
+    #     model_tracked = create_model(
+    #         agent,
+    #         prior,
+    #         data,
+    #         input_cols = :inputs,
+    #         action_cols = :actions,
+    #         grouping_cols = [:ID, :category],
+    #         track_states = true,
+    #     )
+
+    #     #Extract quantities
+    #     agent_parameters, agent_states, statistical_values =
+    #         extract_quantities(fitted_model, model_tracked)
+    # end
+
+    # @testset "missing actions" begin
+
+    #     #Create new dataframe where three actions = missing
+    #     new_data = allowmissing(data, :actions)
+    #     new_data[[2, 7, 12], :actions] .= missing
+
+    #     #Create model
+    #     model = create_model(
+    #         agent,
+    #         prior,
+    #         new_data,
+    #         input_cols = :inputs,
+    #         action_cols = :actions,
+    #         grouping_cols = :ID,
+    #     )
+
+    #     #Fit model
+    #     fitted_model = sample(model, sampler, n_iterations, n_chains = n_chains)
+
+    #     #Rename chains
+    #     renamed_model = rename_chains(fitted_model, prior, data, :ID)
+
+    #     #Create model with tracking states
+    #     model_tracked = create_model(
+    #         agent,
+    #         prior,
+    #         data,
+    #         input_cols = :inputs,
+    #         action_cols = :actions,
+    #         grouping_cols = :ID,
+    #         track_states = true,
+    #     )
+
+    #     #Extract quantities
+    #     agent_parameters, agent_states, statistical_values =
+    #         extract_quantities(fitted_model, model_tracked)
+    # end
+
+    # @testset "custom statistical model" begin
+
+    # end
+
+    @testset "multiple actions" begin
+
+        #Action model with multiple actions    
+        function multi_action(agent, input::Real)
+
+            noise = agent.parameters["noise"]
+
+            actiondist1 = Normal(input, noise)
+            actiondist2 = Normal(input, noise)
+
+            return [actiondist1, actiondist2]
+        end
+        #Create agent
+        new_agent = init_agent(multi_action, parameters = Dict("noise" => 1.0))
+
+        new_prior = Dict("noise" => LogNormal(0.0, 1.0))
+
+        #Create model
+        model = create_model(
+            new_agent,
+            new_prior,
+            data,
+            input_cols = :inputs,
+            action_cols = [:actions, :actions_2],
+            grouping_cols = :ID,
+        )
+
+        #Fit model
+        fitted_model = sample(model, sampler, n_iterations, n_chains = n_chains)
+
+    end
+
+    @testset "multiple inputs" begin
+
+        #Action model with multiple actions    
+        function multi_input(agent, input::Tuple{R,R}) where {R<:Real}
+
+            noise = agent.parameters["noise"]
+
+            input1, input2 = input
+
+            actiondist = Normal(input1, noise)
+
+            return actiondist
+        end
+        #Create agent
+        new_agent = init_agent(multi_input, parameters = Dict("noise" => 1.0))
+
+        new_prior = Dict("noise" => LogNormal(0.0, 1.0))
+
+        #Create model
+        model = create_model(
+            new_agent,
+            new_prior,
+            data,
+            input_cols = [:inputs, :inputs_2],
+            action_cols = :actions,
+            grouping_cols = :ID,
+        )
+
+        #Fit model
+        fitted_model = sample(model, sampler, n_iterations, n_chains = n_chains)
+    end
+
+    @testset "multiple inputs and multiple actions" begin
+
+        #Action model with multiple actions    
+        function multi_input_action(agent, input::Tuple{R,R}) where {R<:Real}
+
+            noise = agent.parameters["noise"]
+
+            input1, input2 = input
+
+            actiondist1 = Normal(input1, noise)
+            actiondist2 = Normal(input2, noise)
+
+            return [actiondist1, actiondist2]
+        end
+        #Create agent
+        new_agent = init_agent(multi_input_action, parameters = Dict("noise" => 1.0))
+
+        new_prior = Dict("noise" => LogNormal(0.0, 1.0))
+
+        #Create model
+        model = create_model(
+            new_agent,
+            new_prior,
+            data,
+            input_cols = [:inputs, :inputs_2],
+            action_cols = [:actions, :actions_2],
+            grouping_cols = :ID,
+        )
+
+        #Fit model
+        fitted_model = sample(model, sampler, n_iterations, n_chains = n_chains)
+    end
+end
+
+
+
+TArray([1 2; 3 4])
+
+
+
+
+# @testset "ensure parameters are reset after fitting" begin
+
+#     agent = premade_agent("binary_rescorla_wagner_softmax", verbose = false)
+
+#     initial_parameters = get_parameters(agent)
+
+#     param_priors = Dict("learning_rate" => Uniform(0, 1))
+
+#     inputs = [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+
+#     actions = give_inputs!(agent, inputs)
+
+#     chains = fit_model(
+#         agent,
+#         param_priors,
+#         inputs,
+#         actions,
+#         n_chains = 1,
+#         n_iterations = 10,
+#         verbose = false,
+#         show_progress = false,
+#     )
+
+#     @test get_parameters(agent) == initial_parameters
+
+# end
+
+
+# @testset "Make sure fitting allows using a custom sampler" begin
+
+#     agent = premade_agent("binary_rescorla_wagner_softmax", verbose = false)
+
+#     param_priors = Dict("learning_rate" => Uniform(0, 1))
+
+#     inputs = [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+
+#     actions = give_inputs!(agent, inputs)
+
+#     rwmh = externalsampler(AdvancedMH.RWMH(10))
+
+#     chains = fit_model(
+#         agent,
+#         param_priors,
+#         inputs,
+#         actions,
+#         n_chains = 1,
+#         n_iterations = 10,
+#         sampler = rwmh,
+#         verbose = false,
+#         show_progress = false,
+#     )
+
+# end
--- test/testsuite/parameter_recovery_tests.jl
+++ test/testsuite/parameter_recovery_tests.jl
@@ -1,114 +1,114 @@
 using Test
-using ActionModels, Distributions, Distributed
+using ActionModels
 
-@testset "parameter recovery" begin
+# @testset "parameter recovery" begin
 
-    @testset "non-parallelizsed" begin
+#     @testset "non-parallelizsed" begin
 
-        #Agent model to do recovery on
-        agent = premade_agent("continuous_rescorla_wagner_gaussian", verbose = false)
+#         #Agent model to do recovery on
+#         agent = premade_agent("continuous_rescorla_wagner_gaussian", verbose = false)
 
-        #Parameters to be recovered
-        parameter_ranges = Dict(
-            "learning_rate" => collect(0:0.1:1),
-            ("initial", "value") => collect(-2:1:2),
-            "action_noise" => collect(0:0.5:3),
-        )
+#         #Parameters to be recovered
+#         parameter_ranges = Dict(
+#             "learning_rate" => collect(0:0.1:1),
+#             ("initial", "value") => collect(-2:1:2),
+#             "action_noise" => collect(0:0.5:3),
+#         )
 
-        #Input sequences to use
-        input_sequence = [[1, 2, 1, 0, 0, 1, 1, 2, 1, 2], [2, 3, 1, 5, 4, 8, 6, 4, 5]]
+#         #Input sequences to use
+#         input_sequence = [[1, 2, 1, 0, 0, 1, 1, 2, 1, 2], [2, 3, 1, 5, 4, 8, 6, 4, 5]]
 
-        #Sets of priors to use
-        priors = [
-            Dict(
-                "learning_rate" => LogitNormal(0, 1),
-                ("initial", "value") => Normal(0, 1),
-                "action_noise" => truncated(Normal(0, 1), lower = 0),
-            ),
-            Dict(
-                "learning_rate" => LogitNormal(0, 0.1),
-                ("initial", "value") => Normal(0, 0.1),
-                "action_noise" => truncated(Normal(0, 0.1), lower = 0),
-            ),
-        ]
+#         #Sets of priors to use
+#         priors = [
+#             Dict(
+#                 "learning_rate" => LogitNormal(0, 1),
+#                 ("initial", "value") => Normal(0, 1),
+#                 "action_noise" => truncated(Normal(0, 1), lower = 0),
+#             ),
+#             Dict(
+#                 "learning_rate" => LogitNormal(0, 0.1),
+#                 ("initial", "value") => Normal(0, 0.1),
+#                 "action_noise" => truncated(Normal(0, 0.1), lower = 0),
+#             ),
+#         ]
 
-        #Times to repeat each simulation
-        n_simulations = 2
+#         #Times to repeat each simulation
+#         n_simulations = 2
 
-        #Sampler settings
-        sampler_settings = (n_iterations = 10, n_chains = 1)
+#         #Sampler settings
+#         sampler_settings = (n_iterations = 10, n_chains = 1)
 
-        #Run parameter recovery
-        results_df = parameter_recovery(
-            agent,
-            parameter_ranges,
-            input_sequence,
-            priors,
-            n_simulations,
-            sampler_settings = sampler_settings,
-            show_progress = false,
-        )
+#         #Run parameter recovery
+#         results_df = parameter_recovery(
+#             agent,
+#             parameter_ranges,
+#             input_sequence,
+#             priors,
+#             n_simulations,
+#             sampler_settings = sampler_settings,
+#             show_progress = false,
+#         )
 
-        @test results_df isa DataFrame
+#         @test results_df isa DataFrame
 
-    end
+#     end
 
-    @testset "parallelised" begin
+#     @testset "parallelised" begin
 
-        addprocs(2, exeflags = "--project")
+#         addprocs(2, exeflags = "--project")
 
-        @everywhere begin
-            using ActionModels, Distributions
+#         @everywhere begin
+#             using ActionModels, Distributions
 
-            #Agent model to do recovery on
-            agent = premade_agent("continuous_rescorla_wagner_gaussian", verbose = false)
+#             #Agent model to do recovery on
+#             agent = premade_agent("continuous_rescorla_wagner_gaussian", verbose = false)
 
-            #Parameters to be recovered
-            parameter_ranges = Dict(
-                "learning_rate" => collect(0:0.1:1),
-                ("initial", "value") => collect(-2:1:2),
-                "action_noise" => collect(0:0.5:3),
-            )
+#             #Parameters to be recovered
+#             parameter_ranges = Dict(
+#                 "learning_rate" => collect(0:0.1:1),
+#                 ("initial", "value") => collect(-2:1:2),
+#                 "action_noise" => collect(0:0.5:3),
+#             )
 
-            #Input sequences to use
-            input_sequence = [[1, 2, 1, 0, 0, 1, 1, 2, 1, 2], [2, 3, 1, 5, 4, 8, 6, 4, 5]]
+#             #Input sequences to use
+#             input_sequence = [[1, 2, 1, 0, 0, 1, 1, 2, 1, 2], [2, 3, 1, 5, 4, 8, 6, 4, 5]]
 
-            #Sets of priors to use
-            priors = [
-                Dict(
-                    "learning_rate" => LogitNormal(0, 1),
-                    ("initial", "value") => Normal(0, 1),
-                    "action_noise" => truncated(Normal(0, 1), lower = 0),
-                ),
-                Dict(
-                    "learning_rate" => LogitNormal(0, 0.1),
-                    ("initial", "value") => Normal(0, 0.1),
-                    "action_noise" => truncated(Normal(0, 0.1), lower = 0),
-                ),
-            ]
+#             #Sets of priors to use
+#             priors = [
+#                 Dict(
+#                     "learning_rate" => LogitNormal(0, 1),
+#                     ("initial", "value") => Normal(0, 1),
+#                     "action_noise" => truncated(Normal(0, 1), lower = 0),
+#                 ),
+#                 Dict(
+#                     "learning_rate" => LogitNormal(0, 0.1),
+#                     ("initial", "value") => Normal(0, 0.1),
+#                     "action_noise" => truncated(Normal(0, 0.1), lower = 0),
+#                 ),
+#             ]
 
-            #Times to repeat each simulation
-            n_simulations = 2
+#             #Times to repeat each simulation
+#             n_simulations = 2
 
-            #Sampler settings
-            sampler_settings = (n_iterations = 10, n_chains = 1)
-        end
+#             #Sampler settings
+#             sampler_settings = (n_iterations = 10, n_chains = 1)
+#         end
 
-        #Run parameter recovery
-        results_df = parameter_recovery(
-            agent,
-            parameter_ranges,
-            input_sequence,
-            priors,
-            n_simulations,
-            sampler_settings = sampler_settings,
-            parallel = true,
-            show_progress = false,
-        )
+#         #Run parameter recovery
+#         results_df = parameter_recovery(
+#             agent,
+#             parameter_ranges,
+#             input_sequence,
+#             priors,
+#             n_simulations,
+#             sampler_settings = sampler_settings,
+#             parallel = true,
+#             show_progress = false,
+#         )
 
-        rmprocs(workers())
+#         rmprocs(workers())
 
-        @test results_df isa DataFrame
+#         @test results_df isa DataFrame
 
-    end
-end
+#     end
+# end
