228973ffbf7170e74eea830ae57198d4d1b4ed3e
Peter Thestrup Waade <peter@waade.net>
2024-09-11 21:16:22 +0100

    (WIP) updating documentation

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,11 @@ using ActionModels, Documenter, Literate
 ## Set paths ##
 actionmodels_path = dirname(pathof(ActionModels))
 
-juliafiles_path = hgf_path * "/docs/julia_files"
+juliafiles_path = actionmodels_path * "/docs/julia_files"
 user_guides_path = juliafiles_path * "/user_guide"
 tutorials_path = juliafiles_path * "/tutorials"
 
-markdown_src_path = hgf_path * "/docs/src"
+markdown_src_path = actionmodels_path * "/docs/src"
 theory_path = markdown_src_path * "/theory"
 generated_user_guide_path = markdown_src_path * "/generated/user_guide"
 generated_tutorials_path = markdown_src_path * "/generated/tutorials"
@@ -40,31 +40,14 @@ for filename in readdir(user_guides_path)
     end
 end
 
-
-
-
-
-for filename in readdir("docs/src/generated_markdown_files")
-    rm("docs/src/generated_markdown_files/" * filename)
-end
-
-#Make Julia source files to markdowns
-for filename in readdir("docs/src/julia_src_files")
+#Generate markdown files for tutorials
+for filename in readdir(tutorials_path)
     if endswith(filename, ".jl")
-        #Place the index file in another folder than the rest of the documentation
-        if startswith(filename, "index")
-            Literate.markdown(
-                "docs/src/Julia_src_files/" * filename,
-                "docs/src",
-                documenter = true,
-            )
-        else
-            Literate.markdown(
-                "docs/src/julia_src_files/" * filename,
-                "docs/src/generated_markdown_files",
-                documenter = true,
-            )
-        end
+        Literate.markdown(
+            tutorials_path * "/" * filename,
+            generated_tutorials_path,
+            documenter = true,
+        )
     end
 end
 
@@ -75,7 +58,7 @@ DocMeta.setdocmeta!(ActionModels, :DocTestSetup, :(using ActionModels); recursiv
 #Create documentation
 makedocs(;
     modules = [ActionModels],
-    authors = "Peter Thestrup Waade ptw@cas.au.dk, Anna Hedvig Møller Daugaard hedvig.2808@gmail.com, Jacopo Comoglio jacopo.comoglio@gmail.com, Christoph Mathys chmathys@cas.au.dk
+    authors = "Peter Thestrup Waade ptw@cas.au.dk, Malte Lau, Anna Hedvig Møller Daugaard hedvig.2808@gmail.com, Jacopo Comoglio jacopo.comoglio@gmail.com, Luke Ring, Christoph Mathys chmathys@cas.au.dk
                   and contributors",
     repo = "https://github.com/ilabcode/ActionModels.jl/blob/{commit}{path}#{line}",
     sitename = "ActionModels.jl",
@@ -85,25 +68,25 @@ makedocs(;
         assets = String[],
     ),
     pages = [
-        "Introduction to Action Models" => [
-            "generated_markdown_files/introduction.md",
-            "generated_markdown_files/agent_and_actionmodel.md",
-            "generated_markdown_files/fitting_vs_simulating.md",
-        ]
-        "Creating Your Model" => [
-            "generated_markdown_files/creating_own_action_model.md",
-            "generated_markdown_files/premade_agents_and_models.md",
-        ]
-        "Agent Based Simulation" => [
-            "generated_markdown_files/simulation_with_an_agent.md",
-            "generated_markdown_files/variations_of_util.md",
-        ]
-        "Fitting an Agent Model" => [
-            "generated_markdown_files/fitting_an_agent_model_to_data.md",
-            "generated_markdown_files/prior_predictive_sim.md",
-        ]
-        "Advanced Usage" => ["generated_markdown_files/custom_fit_model.md"]
-    ],
+        # "Introduction to Action Models" => [
+    #         "generated_markdown_files/introduction.md",
+    #         "generated_markdown_files/agent_and_actionmodel.md",
+    #         "generated_markdown_files/fitting_vs_simulating.md",
+    #     ]
+    #     "Creating Your Model" => [
+    #         "generated_markdown_files/creating_own_action_model.md",
+    #         "generated_markdown_files/premade_agents_and_models.md",
+    #     ]
+    #     "Agent Based Simulation" => [
+    #         "generated_markdown_files/simulation_with_an_agent.md",
+    #         "generated_markdown_files/variations_of_util.md",
+    #     ]
+    #     "Fitting an Agent Model" => [
+    #         "generated_markdown_files/fitting_an_agent_model_to_data.md",
+    #         "generated_markdown_files/prior_predictive_sim.md",
+    #     ]
+    #     "Advanced Usage" => ["generated_markdown_files/custom_fit_model.md"]
+    # ],
 )
 
 deploydocs(;
