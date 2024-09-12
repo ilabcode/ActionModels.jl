636077f1a0b9b7e1f736661a0ab5f76b3192037c
Peter Thestrup Waade <peter@waade.net>
2024-09-12 11:16:45 +0100

    minor flexibility upgrade to fit model

--- src/fitting/fit_model.jl
+++ src/fitting/fit_model.jl
@@ -1,17 +1,16 @@
 #####################################################################
 ### CONVENIENCE FUNCTION FOR DOING FULL MODEL FITTING IN ONE LINE ###
 #####################################################################
-
 function fit_model(
-    model::DynamicPPL.Model,
-    parallelization_type::Union{Nothing,AbstractMCMC.AbstractMCMCEnsemble} = nothing;
+    model::DynamicPPL.Model;
+    parallelization::Union{Nothing,AbstractMCMC.AbstractMCMCEnsemble} = nothing,
     sampler::Union{DynamicPPL.AbstractSampler,Turing.Inference.InferenceAlgorithm} = NUTS(
         -1,
         0.65;
         adtype = AutoReverseDiff(; compile = true),
     ),
     n_iterations::Integer = 1000,
-    n_chains = 2,
+    n_chains = 1,
     show_sample_rejections::Bool = false,
     sampler_kwargs...,
 )
@@ -27,17 +26,10 @@ function fit_model(
     end
 
     ## Fit model ##
-    if !isnothing(parallelization_type)
+    if !isnothing(parallelization)
         #With parallelization
         chains = Logging.with_logger(sampling_logger) do
-            sample(
-                model,
-                sampler,
-                parallelization_type,
-                n_iterations,
-                n_chains;
-                sampler_kwargs...,
-            )
+            sample(model, sampler, parallelization, n_iterations, n_chains; sampler_kwargs...)
         end
     else
         chains = Logging.with_logger(sampling_logger) do
@@ -47,3 +39,31 @@ function fit_model(
 
     return FitModelResults(model, nothing, chains)
 end
+
+
+####################################################################
+### FULL CONVENIENCE FUNCTION THAT CAN ADD ADDITIONAL COMPONENTS ###
+####################################################################
+# function fit_model(
+#     agent::Agent,
+#     statistical_model::Union{M,P},
+#     data::DataFrame;
+#     parallelization::Union{Nothing,AbstractMCMC.AbstractMCMCEnsemble} = nothing,
+#     extract_quantities::Bool = true,
+#     sampler_kwargs...,
+# ) where {M<:DynamicPPL.Model,T<:Union{String,Tuple,Any},D<:Distribution,P<:Dict{T,D}}
+
+#     #Create a full model combining the agent model and the statistical model
+#     model = create_model(agent, statistical_model, data)
+
+#     #Fit the model
+#     results = fit_model(model; parallelization = parallelization, sampler_kwargs...)
+
+#     #Add tracked model
+#     results.tracked_model =
+#         create_model(agent, statistical_model, data, track_states = true)
+
+#     #Extract tracked states
+#     results.agent_parameters, results.agent_states, results.statistical_values =
+#         extract_quantities(results.chains, results.tracked_model)
+# end
